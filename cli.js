7a4da027c41d780fceaf7a2e910961dfeb634da5
Levi Lucas <levilucasaraujolopes@gmail.com>
2022-06-23 18:28:14 -0300

    Atualizando Projeto

---
+++ cli.js
@@ -0,0 +1,18 @@
+//comand line interface
+import chalk from 'chalk';
+import pegaArquivo from './index.js'
+import validaURLs from './http-validacao.js'
+
+const caminho = process.argv;
+
+async function processaTexto(caminhoDoArquivo) {
+    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
+    if (caminho[3] === 'validar') {
+        console.log(chalk.yellow('Links validados'), await validaURLs(resultado));
+    } else {
+        console.log(chalk.yellow('Lista de Links'), resultado);
+    }
+
+}
+
+processaTexto(caminho);
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import chalk from 'chalk';
 import * as fs from 'node:fs';
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList)que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
 function extraiLinks(texto) {
     const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const linksExtraidos = texto.match(regex);
-    console.log(linksExtraidos);
+    const arrayResultados = [];
+    let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados.length === 0 ? 'Não há links' : arrayResultados;
 }
 
-extraiLinks(texto);
 
 function trataErro(erro) {
     throw new Error(chalk.black.bgBlue(erro.code, 'O caminho é um diretorio'));
@@ -19,16 +20,15 @@ async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        return extraiLinks(texto);
     } catch{
         trataErro(erro);
-    } finally {
-        console.log(chalk.yellow('Operação concluida'));
+
     }
 }
 
+export default pegaArquivo;
 
-//pegaArquivo('./texto1.md');
 
 //function pegaArquivo(caminhoDoArquivo) {
 //    const encoding = 'utf-8'
